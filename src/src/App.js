df78d91c6d9f9957d17e4bfeb5cb96d9b9012b81
Naufal <112184757+blckdfly@users.noreply.github.com>
2024-09-24 10:45:33 +0700

    Add files via upload

---
+++ src/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import logo from './ocarbn-logo.png';
+
+
+function App() {
+  return (
+    <div className="coming-soon">
+      <div className="content">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>Coming Soon</h1>
+        <p>We're working hard to bring you something amazing. Stay tuned!</p>
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
