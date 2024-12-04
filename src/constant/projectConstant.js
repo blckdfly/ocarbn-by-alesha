6aca0c5344d862d1c78f5016ff1daeb220e1e88c
xrt19 <122961586+xrt19@users.noreply.github.com>
2024-12-04 12:00:14 +0700

    Update projectConstant.js
    perbaikan dibantu kak andien

--- src/constant/projectConstant.js
+++ src/constant/projectConstant.js
@@ -1,3 +1,8 @@
+import image1 from '../assets/project/project1.png';
+import image2 from '../assets/project/project2.png';
+import image3 from '../assets/project/project3.png';
+import image4 from '../assets/project/project4.png';
+
 export const community = {
     community1: require('../assets/project/community1.png'),
     community2: require('../assets/project/community2.png'),
@@ -9,25 +14,25 @@ export const community = {
       title: 'Alesha',
       description: 'Mangroves conservation at critical areas with community participation.',
       date: '19 Jun 2025',
-      img: require('../assets/project/project1.png'),
+      img: image1,
     },
     project2: {
       title: 'Varuna',
       description: 'Seagrass restoration with cutting-edge and data-driven monitoring.',
       date: '19 Jun 2025',
-      img: require('../assets/project/project2.png'),
+      img: image2,
     },
     project3: {
       title: 'Aneira',
       description: 'Mangroves restoration in coastal ecosystems with carbon benefits.',
       date: '19 Jun 2025',
-      img: require('../assets/project/project3.png'),
+      img: image3,
     },
     project4: {
       title: 'Eulalie',
       description: 'Rehabilitation of degraded mangrove ecosystems for coastal protection.',
       date: '19 Jun 2025',
-      img: require('../assets/project/project4.png'),
+      img: image4,
     },
   };
   
