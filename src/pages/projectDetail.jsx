68f57bf5e9621fcab778928f4f4906534d18c575
xrt19 <122961586+xrt19@users.noreply.github.com>
2024-12-22 16:44:57 +0700

    Update projectDetail.jsx

--- src/pages/projectDetail.jsx
+++ src/pages/projectDetail.jsx
@@ -8,32 +8,45 @@ const ProjectDetail = () => {
 
   return (
     <div className="project-detail-container">
+        <div className="gradient-circle"></div>
+    
+<div className="project-list-section">
+  {projects.map((project) => (
+    <div
+      key={project.id}
+      className="project-card"
+      style={{ backgroundImage: `url(${project.img})` }}
+    >
+      <div className="project-info">
+        <h3>{project.name}</h3>
+        <h5>{project.type}</h5>
+        
+        <p className="project-description">{project.description}</p>
 
-      <div className="gradient-circle"></div>
-      
-      <div className="project-list-section">
-        {projects.map((project) => (
-          <div key={project.id} className="project-card">
-            <img src={project.img} alt={project.name} />
-            <div className="project-info">
-              <h3>{project.name}</h3>
-              <p>{project.type}</p>
-              <div className="project-date">
-                <span>{project.date} CO₂</span>
-                <span>{project.year}</span>
-              </div>
-              <p className="project-description">{project.description}</p>
-
-              <button
-                className="get-in-touch"
-                onClick={() => navigate(`/detail-conservation/${project.id}`)}
-              >
-                Get in Touch
-              </button>
-            </div>
+        <div className="project-date">
+          <div className='project-date-left'>
+            <h4>{project.date} tCO₂</h4> 
+            <h6>Annual removal capacity</h6>
           </div>
-        ))}
+          <div className='project-date-right'>
+            <h4>{project.year}</h4>
+            <h6>scheduled</h6>
+          </div>
+
+
+        </div>
+
+        <button
+          className="get-in-touch"
+          onClick={() => navigate(`/detail-conservation/${project.id}`)}
+        >
+          Get in Touch
+        </button>
       </div>
+    </div>
+  ))}
+</div>
+
     </div>
   );
 };
